e992d97178a3f3386e0dddd31e3889c1f76e234e
leng <leng@DESKTOP-JLH7JN1>
2019-09-10 13:03:09 +0800

    提交

--- fuzhang1/src/com/happy/advice/LogAnnotation.java
+++ fuzhang1/src/com/happy/advice/LogAnnotation.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+//2113
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface LogAnnotation {
